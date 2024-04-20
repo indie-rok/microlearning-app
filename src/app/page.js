7561b6e39ae56b75ce666f7e6e2d1c877e8d7b80
Emmanuel Orozco <indi3.rok@gmail.com>
2024-04-20 18:17:12 +0200

    Added homepage

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,72 @@
-import Image from "next/image";
+"use client";
+
+import {
+  Divider,
+  Row,
+  Col,
+  Card,
+  Alert,
+  Button,
+  Space,
+  Typography,
+} from "antd";
+import Meta from "antd/es/card/Meta";
+import { useRouter } from "next/navigation";
+
 import styles from "./page.module.css";
 
-export default function Home() {
-    return (
-        <p>
+const style = {
+  background: "#0092ff",
+  padding: "8px 0",
+};
 
-        </p>
-    );
+export default function Home() {
+  const { push } = useRouter();
+  return (
+    <>
+      <Alert
+        message="Congratz on your 10 day streak"
+        type="success"
+        action={
+          <Space direction="vertical">
+            <Button size="small" type="primary">
+              Get my 100 coins
+            </Button>
+          </Space>
+        }
+        closable
+      />
+      <Divider />
+      <Typography.Title level={3}>My courses</Typography.Title>
+
+      <Row
+        gutter={{
+          xs: 8,
+          sm: 16,
+          md: 24,
+          lg: 32,
+        }}
+      >
+        <Col className="gutter-row" span={6}>
+          <Card
+            hoverable
+            style={{
+              width: 240,
+            }}
+            onClick={() => {
+              push("courses/sql");
+            }}
+            cover={
+              <img
+                alt="example"
+                src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
+              />
+            }
+          >
+            <Meta title="SQL 101" />
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 }
